e89986086957d8c27fa03e08cef50b6e8032c58f
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2020-10-06 19:41:14 +0300

    git init

---
+++ RushSDK/RushSDK.h
@@ -0,0 +1,18 @@
+//
+//  RushSDK.h
+//  RushSDK
+//
+//  Created by Andrey Chernyshev on 06.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RushSDK.
+FOUNDATION_EXPORT double RushSDKVersionNumber;
+
+//! Project version string for RushSDK.
+FOUNDATION_EXPORT const unsigned char RushSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RushSDK/PublicHeader.h>
+
+
